ab28246fc8d61f3bb24f7fc847289caa7551ef85
Yngwie Kurebayashi <yngwie.rs2@gmail.com>
2025-02-24 23:32:32 +0000

    feat: remove tarnsition inutil

--- src/Modules/Home/style.ts
+++ src/Modules/Home/style.ts
@@ -10,5 +10,4 @@ export const MainMapContainer = styled(MapContainer)<{ $hasInfo: boolean }>`
   height: 90vh;
   width: ${(props) => (props.$hasInfo ? "70%" : "100%")};
   position: relative;
-  transition: 5s ease-in-out;
 `;
